3d5319a6c658261f3640fa491d6a4b321b8ae190
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2024-01-24 16:04:43 +0530

    useContext based project

---
+++ themeSwitcher/src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./component/Card";
+import ThemeBtn from "./component/Themebtn";
+import { ThemeProvider } from "./component/context/Theme";
+
+function App() {
+const [themeMode, setthemeMode] = useState('light')
+const lightTheme=()=>{
+  setthemeMode('light')
+}
+const darkTheme=()=>{
+  setthemeMode('dark')
+}
+useEffect(() => {
+  document.querySelector('html').classList.remove('light', 'dark')
+  document.querySelector('html').classList.add(themeMode)
+}, [themeMode])
+
+
+  return (
+    <ThemeProvider value={{lightTheme, darkTheme, themeMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ themeSwitcher/src/component/context/Theme.js
@@ -0,0 +1,13 @@
+import { useContext, createContext } from "react";
+
+export const ThemeContext = createContext({
+    lightTheme: ()=>{},
+    darkTheme: ()=>{},
+    themeMode: 'light'
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
